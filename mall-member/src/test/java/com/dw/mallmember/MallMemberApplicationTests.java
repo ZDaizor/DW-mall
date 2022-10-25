cd269baa995c02ae17ecce5679b5b4fd910218e3
Z Daizor <xingzuozhou@gmail.com>
2022-10-25 22:07:29 +0800

    project init

---
+++ mall-member/src/test/java/com/dw/mallmember/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw.mallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
